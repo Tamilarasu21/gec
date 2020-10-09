a50158b14dfbad1c9026821ebd0260de3cd01c3b
Tamilarasu21 <tamilarasuat1998@gmail.com>
2020-10-09 22:06:41 +0530

    initial commit

---
+++ js/validation.js
@@ -0,0 +1,24 @@
+function checkPass()
+{
+    var password = document.getElementById('password');
+    var cpassword  = document.getElementById('cpassword');
+
+    if(password.value=="" && cpassword.value=="")
+    {
+        cpassword.style.border = "1px solid skyblue";
+        cpassword.style.outline = "none";
+        cpassword.style.boxShadow = "1px 1px 3px skyblue";
+    }
+    else if(password.value == cpassword.value)
+    {
+        cpassword.style.border = "1px solid green";
+        cpassword.style.outline = "none";
+        cpassword.style.boxShadow = "1px 1px 3px green";
+    }
+    else
+    {    
+        cpassword.style.border = "1px solid red";
+        cpassword.style.outline = "none";
+        cpassword.style.boxShadow = "1px 1px 3px red";
+    }
+}  
